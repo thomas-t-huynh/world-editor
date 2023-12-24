a17392f77fe9ae1b9c2bf304426ed4e4ff0db28c
unknown <t_huynh927@yahoo.com>
2023-12-24 15:37:25 -0800

    intersections detectin

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,16 +1,36 @@
 class Envelope {
-  constructor(skeleton, width) {
+  constructor(skeleton, width, roundness = 1) {
     this.skeleton = skeleton;
-    this.poly = this.#generatePolygon(width);
+    this.poly = this.#generatePolygon(width, roundness);
   }
 
-  #generatePolygon(width) {
+  #generatePolygon(width, roundness) {
     const { p1, p2 } = this.skeleton;
 
+    // TODO - learn about the math here
     const radius = width / 2;
-    const alpha = Math.atan2(p1.y - p2.y, p1.x - p2.x);
-    const alpha_cw = alpha + Math.PI / 2;
-    const alpha_ccw = alpha - Math.PI / 2;
-    const p1_ccw = translate(p1, alpha_ccw, radius);
+    const alpha = angle(subtract(p1, p2)); // angle between p1 and p2.
+    const alpha_cw = alpha + Math.PI / 2; // angle offset by 90 degrees clockwise
+    const alpha_ccw = alpha - Math.PI / 2; // angle offset by 90 degrees counterclockwise
+    // const p1_ccw = translate(p1, alpha_ccw, radius); // point p1 offset by 90 degrees counterclockwise
+    // const p2_ccw = translate(p2, alpha_ccw, radius); // point p2 offset by 90 degrees counterclockwise
+    // const p2_cw = translate(p2, alpha_cw, radius);
+    // const p1_cw = translate(p1, alpha_cw, radius); // and so on for clockwise
+    const points = [];
+    const step = Math.PI / Math.max(1, roundness);
+    const eps = step / 2; //  lets following loops extra space to reach alpha_cw
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
+      points.push(translate(p1, i, radius));
+    }
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
+      points.push(translate(p2, Math.PI + i, radius));
+    }
+
+    return new Polygon(points);
+  }
+
+  draw(ctx) {
+    this.poly.draw(ctx);
+    this.poly.drawSegments(ctx);
   }
 }
--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -1,6 +1,38 @@
 class Polygon {
   constructor(points) {
     this.points = points;
+    this.segments = [];
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(
+        new Segment(points[i - 1], points[i % points.length]) // modulo is used to wrap around
+      );
+    }
+  }
+
+  static break(poly1, poly2) {
+    const segs1 = poly1.segments;
+    const segs2 = poly2.segments;
+    segs1.forEach((seg1, i) => {
+      segs2.forEach((seg2, j) => {
+        const int = getIntersection(seg1.p1, seg1.p2, seg2.p1, seg2.p2);
+
+        if (int && int.offset !== 1 && int.offset !== 0) {
+          const point = new Point(int.x, int.y);
+          let aux = seg1.p2;
+          seg1.p2 = point;
+          segs1.splice(i + 1, 0, new Segment(point, aux));
+          aux = seg2.p2;
+          seg2.p2 = point;
+          segs2.splice(j + 1, 0, new Segment(point, aux));
+        }
+      });
+    });
+  }
+
+  drawSegments(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx, { color: getRandomColor(), width: 5 });
+    }
   }
 
   draw(
